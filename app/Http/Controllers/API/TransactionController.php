f1ed13b890c70b58541466a6dff9650ab802e947
Wahyu Adhe Triwibowo <adhedewa1996@gmail.com>
2020-10-12 12:32:47 +0700

    test

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -101,10 +101,10 @@ class TransactionController extends BaseController
     public function getPinjam(Request $request){
       $user = $request->user();
       $transaction = Transaction::with('books')
-        ->where([['user_id', $user->id], ['status', 'pinjam']])
+        ->where([['user_id', $user->user_id], ['status', 'pinjam']])
         ->orderBy('id', 'DESC')
         ->get();
-      return $this->sendResponse($user->id, 'Pinjam Berhasil.');
+      return $this->sendResponse($user->user_id, 'Pinjam Berhasil.');
     }
     /**
      * Display a listing of the resource.
